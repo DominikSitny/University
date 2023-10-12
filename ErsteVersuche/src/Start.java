4f66a15af0087bf93a933a0c36f5fb360f2c6d25
DominikSitny <dominik.sitny@gmail.com>
2023-10-13 00:34:29 +0200

    Fertig 12.10.23

--- ErsteVersuche/src/Start.java
+++ ErsteVersuche/src/Start.java
@@ -1,10 +1,18 @@
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
 
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testw");
+		StdOut.print("Dieses Programm addiert zwei ganze Zahlen.");
+		StdOut.print("Bitte erste Zahl eingeben: ");
+		int n1 = StdIn.readInt();
+		StdOut.print("Bitte zweite Zahl eingeben: ");
+		int n2 = StdIn.readInt();
+		int sum = n1 + n2;
+		StdOut.print("Die Summe ist " + sum);
 
 	}
 
