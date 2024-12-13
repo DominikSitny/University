fe0263e29e481e2016641cdae3b7db58b9553732
DominikSitny <dominik.sitny@gmail.com>
2024-12-13 13:37:51 +0100

    MVC deleted

--- Drittes_Semester/UebungKlausur/MVC/src/Uebungsblatt4/aufgabe3/Programmierbeispiel/view/IObserverInterface.java
+++
@@ -1,7 +0,0 @@
-package Uebungsblatt4.aufgabe3.Programmierbeispiel.view;
-
-public interface IObserverInterface {
-
-	public void update();
-	
-}
--- Drittes_Semester/UebungKlausur/MVC/src/Uebungsblatt4/aufgabe3/ProgrammierbeispielMitObserver/Controller/IControllerInterface.java
+++
@@ -1,7 +0,0 @@
-package Uebungsblatt4.aufgabe3.ProgrammierbeispielMitObserver.Controller;
-
-public interface IControllerInterface {
-
-	public void setValues (String red, String green, String blue); 
-	
-}
